2a0fa3f192f946750febe610bfcd653ca702c2be
Anatoly Muntean <am@inlead.dk>
2013-03-22 11:30:26 +0200

    case 2359 - Show availability table on issue hover, reserve on issue click.

--- ding_periodical.js
+++ ding_periodical.js
@@ -5,7 +5,7 @@
 
   Drupal.behaviors.ding_periodical = {
     attach: function (context, settings) {
-      $('.field-name-ding-periodical-issues .ding-periodical-fold').toggle(function() {
+      $('.field-name-ding-periodical-issues .ding-periodical-fold.expand').toggle(function() {
         $(this).next().show();
         $('.page-ting-object .panel-pane.pane-ting-object-ding-availability-holdings').addClass('holdings-collapsed');
         $('.page-ting-object .grid-3 .panel-pane.pane-ting-object-ding-entity-buttons').addClass('holdings-collapsed');
@@ -16,6 +16,15 @@
         $(this).next().hide();
         $(this).next().toggleClass('expanded-periodicals');
         $(this).parent().toggleClass('expanded-periodicals-parent');
+      });
+
+
+      $('.ding-periodical-issue').mouseover(function() {
+        $(this).next().show();
+      });
+
+      $('.ding-periodical-issue').mouseout(function() {
+        $(this).next().hide();
       });
     }
   }
